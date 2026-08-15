c756679b64c54adf519a883002ec2db7c89e6a59
Shreyas Rajashekar <rajshekarshreyas26@gmail.com>
2026-08-15 10:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.44%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def recoverOrder(self, order, friends):
+        """
+        :type order: List[int]
+        :type friends: List[int]
+        :rtype: List[int]
+        """
+        l=[]
+        for i in order:
+            if i in friends:
+                l.append(i)
+        return l
